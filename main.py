555a3a13a5f76d7c7f8d2fa9ea5e44f59ef59cf2
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-21 16:40:27 -0500

    main1

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
 from fastapi import FastAPI, HTTPException, Depends, status, Query
 from typing import List, Optional
+
+# Importar módulos con nombres en español
+import crud
+import modelos # Importa los modelos Pydantic en español
+from modelos import Juego, JuegoCrear, Desarrollador, DesarrolladorCrear
