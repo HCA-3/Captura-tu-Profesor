2b80286d71fe49807a318dce5818dfa3da5ef4d4
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-21 16:01:06 -0500

    modelos8

--- modelos.py
+++ modelos.py
@@ -1,7 +1,9 @@
+# modelos.py
 from pydantic import BaseModel, Field
 from typing import Optional, List
 
 # --- Modelo Desarrollador ---
+
 class DesarrolladorBase(BaseModel):
     nombre: str = Field(..., example="Naughty Dog")
     pais: Optional[str] = Field(None, example="EEUU")
@@ -13,10 +15,13 @@ class DesarrolladorCrear(DesarrolladorBase):
 class Desarrollador(DesarrolladorBase):
     id: int
     esta_eliminado: bool = False # Para borrado lógico
-    
- class Config:
+
+    class Config:
         orm_mode = True # Permite compatibilidad con ORMs, útil aquí para estructura
         allow_population_by_field_name = True # Permite usar 'ano_fundacion' en lugar de 'ano_fundacion' si es necesario
+
+
+# --- Modelo Juego ---
 
 class JuegoBase(BaseModel):
     titulo: str = Field(..., example="The Last of Us Parte II")
@@ -31,7 +36,7 @@ class JuegoCrear(JuegoBase):
 class Juego(JuegoBase):
     id: int
     esta_eliminado: bool = False # Para borrado lógico
-    
-class Config:
+
+    class Config:
         orm_mode = True
         allow_population_by_field_name = True
