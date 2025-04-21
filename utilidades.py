671beec3536f744b7b07b52b2d769b5fd54151a2
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-21 16:02:50 -0500

    utilidades1

--- utilidades.py
+++ utilidades.py
@@ -2,3 +2,9 @@ import time
 
 # Una forma simple de generar IDs (en una app real usarías UUIDs o IDs de BD)
 _ultimo_id = int(time.time() * 1000)
+
+def obtener_siguiente_id() -> int:
+    """Genera un ID numérico simple y secuencial."""
+    global _ultimo_id
+    _ultimo_id += 1
+    return _ultimo_id
