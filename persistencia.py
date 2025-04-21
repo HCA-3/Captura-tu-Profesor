9a27889d507c87866f5472675a1175478044f8d5
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-21 16:11:00 -0500

    persistencia1

--- persistencia.py
+++ persistencia.py
@@ -4,3 +4,10 @@ from typing import List, Dict, Any
 from fastapi import HTTPException, status
 # Asegúrate que la importación de modelos use los nombres en español
 from modelos import Juego, Desarrollador
+
+DIRECTORIO_DATOS = "datos" # Directorio cambiado a español
+ARCHIVO_JUEGOS = os.path.join(DIRECTORIO_DATOS, "juegos.csv")
+ARCHIVO_DESARROLLADORES = os.path.join(DIRECTORIO_DATOS, "desarrolladores.csv")
+
+# Asegurarse de que el directorio de datos exista
+os.makedirs(DIRECTORIO_DATOS, exist_ok=True)
