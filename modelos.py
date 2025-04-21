bc8d0d4748a5710b92410d7ddb779b6461b22e0b
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-21 15:54:05 -0500

    modelos1

--- modelos.py
+++ modelos.py
@@ -2,3 +2,7 @@ from pydantic import BaseModel, Field
 from typing import Optional, List
 
 # --- Modelo Desarrollador ---
+class DesarrolladorBase(BaseModel):
+    nombre: str = Field(..., example="Naughty Dog")
+    pais: Optional[str] = Field(None, example="EEUU")
+    ano_fundacion: Optional[int] = Field(None, example=1984)
