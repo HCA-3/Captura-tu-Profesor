fa9c5ba026dae0695eeab275bd6d558183c0c7fd
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-21 16:19:45 -0500

    crud1

--- crud.py
+++ crud.py
@@ -1,3 +1,11 @@
 # crud.py
 from typing import List, Optional, Dict, Any
 from fastapi import HTTPException, status
+
+# Importar modelos, persistencia y utilidades con nombres en español
+import modelos
+from modelos import Juego, JuegoCrear, Desarrollador, DesarrolladorCrear
+from persistencia import (
+    cargar_juegos, guardar_juegos,
+    cargar_desarrolladores, guardar_desarrolladores
+)
