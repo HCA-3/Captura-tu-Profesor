142c446b3df6fb057e90d07f6479cbd2496c4121
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-21 15:55:03 -0500

    modelos2

--- modelos.py
+++ modelos.py
@@ -6,3 +6,6 @@ class DesarrolladorBase(BaseModel):
     nombre: str = Field(..., example="Naughty Dog")
     pais: Optional[str] = Field(None, example="EEUU")
     ano_fundacion: Optional[int] = Field(None, example=1984)
+
+class DesarrolladorCrear(DesarrolladorBase):
+    pass # No necesita campos adicionales para la creación
