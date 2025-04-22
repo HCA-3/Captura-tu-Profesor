b74d93ee0ec62edaaf9d12ac2784e505645bac5b
DAVIDHCA3 <dscastelblanco82@ucatolica.edu.co>
2025-04-22 11:33:59 -0500

    arreglos15

--- modelos.py
+++ modelos.py
@@ -1,20 +1,37 @@
 from pydantic import BaseModel, Field
 from typing import Optional, List
 
+class DesarrolladorBase(BaseModel):
+    nombre: str = Field(..., example="Naughty Dog")
+    pais: Optional[str] = Field(None, example="EEUU")
+    ano_fundacion: Optional[int] = Field(None, example=1984)
+
+class DesarrolladorCrear(DesarrolladorBase):
+    pass 
+
+class Desarrollador(DesarrolladorBase):
+    id: int
+    esta_eliminado: bool = False 
+
+    class Config:
+        orm_mode = True 
+        allow_population_by_field_name = True 
+
+
 class JuegoBase(BaseModel):
-    titulo: str = Field(..., example="Elden Ring")
-    genero: str = Field(..., example="ARPG")
-    plataformas: List[str] = Field(default_factory=list, example=["PC", "PS5", "Xbox Series X"])
-    ano_lanzamiento: Optional[int] = Field(None, example=2022)
-    nombre_desarrollador: Optional[str] = Field(None, example="FromSoftware")
+    titulo: str = Field(..., example="The Last of Us Parte II")
+    genero: str = Field(..., example="Acción-Aventura")
+    plataformas: List[str] = Field(default_factory=list, example=["PlayStation 4", "PlayStation 5"])
+    ano_lanzamiento: Optional[int] = Field(None, example=2020)
+    desarrollador_id: int = Field(..., example=1) 
 
 class JuegoCrear(JuegoBase):
     pass 
 
 class Juego(JuegoBase):
     id: int
-    esta_eliminado: bool = False 
+    esta_eliminado: bool = False
 
     class Config:
         orm_mode = True
-        allow_population_by_field_name = True 
+        allow_population_by_field_name = True
